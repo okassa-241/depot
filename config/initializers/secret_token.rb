63476b485c2d03edfad37fa6d85e1b9c77b99c2e
U-OlympeKassa-PC\Olympe Kassa <Olympe Kassa@OlympeKassa-PC.(none)>
2012-11-25 20:45:29 +0100

    * Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '62476e1dfeab931d7c5d42540232dcc39c9bd6faac5f5a710fca3c5af98500c029ac394b29004c510edab2c409bc8607d8822ef998d14f58663131bd7f1cbb77'
